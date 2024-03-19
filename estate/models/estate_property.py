1f9ad8d23d8aecfb11cca1a0736c4ae3cebab381
Frederik Bezuidenhout <fbez1997@gmail.com>
2024-03-19 10:46:03 +1000

    Add new fields to property class. Update field characteristics

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,11 +7,19 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
+    state = fields.Selection(
+        string = 'State',
+        selection = [('new', 'New'), ('offer received', 'Offer Received'), ('offer accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        help="Current status of property listing",
+        required=True,
+        copy=False,
+        default='new',
+    )
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=fields.Datetime.add(fields.Datetime.today(), months=+3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,4 +28,6 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string = 'Garden Orientation',
         selection =[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-        help="Orientation of the Garden")
+        help="Orientation of the Garden",
+    )
+    active = fields.Boolean(default=True)
