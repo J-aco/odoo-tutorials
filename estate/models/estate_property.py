545e55a94b4dc826d7029586b533ad91f2b0bad5
Frederik Bezuidenhout <fbez1997@gmail.com>
2024-03-21 15:27:57 +1000

    add compute fields for best offer and total property area

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -43,4 +43,10 @@ class EstateProperty(models.Model):
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price'))
     
